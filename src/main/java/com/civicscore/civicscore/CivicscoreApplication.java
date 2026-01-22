52def1eb5c8fe95728129a6c05979eacc19511ab
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-01-22 07:52:57 +0530

    Ganapati Bappa Moraya

---
+++ src/main/java/com/civicscore/civicscore/CivicscoreApplication.java
@@ -0,0 +1,13 @@
+package com.civicscore.civicscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicscoreApplication.class, args);
+	}
+
+}
