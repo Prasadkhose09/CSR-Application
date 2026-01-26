88223a0a448ea23033f56e3f31b61aa18592cd49
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-01-26 21:22:39 +0530

    Resolved

--- src/main/java/com/civicscore/CivicscoreApplication.java
+++ src/main/java/com/civicscore/CivicscoreApplication.java
@@ -2,8 +2,10 @@ package com.civicscore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.civicscore")
 public class CivicscoreApplication {
 
 	public static void main(String[] args) {
