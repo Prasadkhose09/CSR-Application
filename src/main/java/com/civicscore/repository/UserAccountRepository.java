a75dcea2ffc75acaefc0d96a178fbcff5c68dba0
Prasadkhose09 <Prasadkhose512@gmail.com>
2026-05-13 22:09:25 +0530

    Fixed- Adding User Account + DB Authentication

--- src/main/java/com/civicscore/repository/UserAccountRepository.java
+++ src/main/java/com/civicscore/repository/UserAccountRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-    Optional<UserAccount> findByUserName(String username);
+    Optional<UserAccount> findByUsername(String username);
 }
